3b92894d2de0bc977737c4cd0082d6884a391c92
Aishwarya Allam <aa362@njit.edu>
2021-03-21 23:23:24 -0400

    added testing for division

--- main.py
+++ main.py
@@ -84,6 +84,24 @@ class TestCalC(unittest.TestCase):
             self.assertEqual(self.calculator.multiplication(a[i], b[i]), ans[i])
         print("Multiplication all test case passed")
 
+    def test_division(self):
+        a = []
+        b = []
+        ans = []
+        with open('Unit Test Division.csv') as file:
+            csv_reader = csv.reader(file, delimiter=',')
+            line = 0
+            for row in csv_reader:
+                if line != 0:
+                    a.append(float(row[0]))
+                    b.append(float(row[1]))
+                    ans.append(float(row[2]))
+                line += 1
+
+        for i in range(0, len(a)):
+            self.assertEqual(round(self.calculator.division(b[i], a[i]), 2), round(ans[i], 2))
+        print("Division all test case passed")
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)

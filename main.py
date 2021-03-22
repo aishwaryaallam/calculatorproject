ca76961e2faefdf24fa6762864faeb723f5bb8b3
Aishwarya Allam <aa362@njit.edu>
2021-03-21 23:21:36 -0400

    added testing for subtraction

--- main.py
+++ main.py
@@ -48,6 +48,24 @@ class TestCalC(unittest.TestCase):
             self.assertEqual(self.calculator.addition(a[i], b[i]), ans[i])
         print("Addition all test case passed")
 
+    def test_subtraction(self):
+        a = []
+        b = []
+        ans = []
+        with open('Unit Test Subtraction.csv') as file:
+            csv_reader = csv.reader(file, delimiter=',')
+            line = 0
+            for row in csv_reader:
+                if line != 0:
+                    a.append(float(row[0]))
+                    b.append(float(row[1]))
+                    ans.append(float(row[2]))
+                line += 1
+
+        for i in range(0, len(a)):
+            self.assertEqual(self.calculator.subtraction(b[i], a[i]), ans[i])
+        print("Subtraction all test case passed")
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
